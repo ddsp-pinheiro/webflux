11602b70874ca8b6313a6ab15bc2b86696505eb8
Daniele Pinheiro <ddsp.pinheiro@gmail.com>
2022-06-01 18:52:53 -0300

    first commit

---
+++ src/main/java/com/pocwebflux/PocwebfluxApplication.java
@@ -0,0 +1,13 @@
+package com.pocwebflux;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class PocwebfluxApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(PocwebfluxApplication.class, args);
+	}
+
+}
